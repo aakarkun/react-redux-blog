b1a35f144d36ba9327e14342497ae9969ce35b0b
aakarkun <aakar.kun@hotmail.com>
2017-11-29 03:01:52 +0545

    Fixed #3

--- server/index.js
+++ server/index.js
@@ -3,11 +3,19 @@ import path from 'path';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
 let app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    hot: true,
+    noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
